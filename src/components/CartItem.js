a4dab035d0da77ecd35d7ec031289bd712585bed
Rajnish Giri <v-ragiri@microsoft.com>
2022-03-12 12:44:25 +0530

    fixed warning

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,7 +5,7 @@ function CartItem({item}){
 
     return <div className="w-full flex-col shadow-md py-2 px-2">
         <div className="flex items-center">
-            <img className="w-4 h-4 rounded-full mr-2" src={item.image} alt='item image'/>
+            <img className="w-4 h-4 rounded-full mr-2" src={item.image} alt='item'/>
         <span className="w-2/4 font-bold">{item.name}</span> <span className="w-1/4 px-2 text-xs">X{item.qnty}</span><button className="w-1/6 bg-blue-600 text-white rounded-sm my-2 text-xs py-1 del" onClick={removeFromCart}>X</button>
         </div>
         <p>{item.note}</p>
